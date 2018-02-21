fa00eddc092f02360b97f2ec6100711078862058
kurtismo <kurtischapman1997@gmail.com>
2018-02-21 12:13:56 +0000

    Grid drawn

--- app/src/main/java/com/lonespark/lightit/MainActivity.java
+++ app/src/main/java/com/lonespark/lightit/MainActivity.java
@@ -1,7 +1,10 @@
 package com.lonespark.lightit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("I did it!");
+        Button btnPlay = findViewById(R.id.btnPlay);
+        btnPlay.setOnClickListener( new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
