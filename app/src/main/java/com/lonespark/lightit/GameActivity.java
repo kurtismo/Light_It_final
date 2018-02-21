d5d06a30d3e1650f7114c3e50510076fc38a0053
kurtismo <kurtischapman1997@gmail.com>
2018-02-21 11:07:33 +0000

    Layout file fixed

--- app/src/main/java/com/lonespark/lightit/GameActivity.java
+++ app/src/main/java/com/lonespark/lightit/GameActivity.java
@@ -3,14 +3,11 @@ package com.lonespark.lightit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        System.out.println("I did it!");
-
+        setContentView(R.layout.activity_game);
     }
 }
